a079d7bd5f395fad422a40d372ca107b1e24619b
Kingnick253 <nicholaswebb2016@gmail.com>
2022-09-25 12:47:18 -0700

    Finished writing Psuedo code

---
+++ index.js
@@ -0,0 +1,33 @@
+const require = require('inquirer');
+
+
+// gather manager data 
+
+
+    //inquirer.prompt()
+
+    //  Then build a manager object
+// Gather engineer data
+
+
+    //  build a engineer object
+
+    //inquirer.prompt()
+
+    //  Then build a engineer object
+// Gather intern data
+
+    //  build a intern object
+
+    //inquirer.prompt()
+    
+    //  Then build a INtern object
+
+// Ask what team memeber they want to add or "are they done"
+
+
+    //inquirer.prompt()
+
+    // Then deciding which function to call() 
+
+//Generate Html file
